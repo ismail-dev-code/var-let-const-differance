5505853258201d3eee6e2448bbf3cf7a26ca918c
ismail-dev-code <ismail.online0385678@gmail.com>
2024-11-27 22:30:25 +0600

    template added

---
+++ template.js
@@ -0,0 +1,24 @@
+// one way 
+const first = 'tia'
+const last = 'pakhi'
+const greet = 'kaissa kaissa'
+
+const name = first +' '+ last +' '+ greet;
+
+console.log(name);
+// one way 
+const a = 10;
+const b = 30;
+const result = 'The sum of ' + a + ' and ' + b + ' is ' + (a+b);
+console.log(result);
+
+// more easy way 
+const math = `The sum of ${a} and ${b} is ${a+b}`
+console.log(math); 
+
+const text =   `faisal
+i am a web developer`
+console.log(text);
+
+const message = 'Faisal \n i am developer'
+console.log(message);
