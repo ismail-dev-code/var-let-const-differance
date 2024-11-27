7fa7973702d6b0888966ef31c69808f1d770f749
ismail-dev-code <ismail.online0385678@gmail.com>
2024-11-27 10:46:54 +0600

    differance of var let const

---
+++ var-let-const.js
@@ -0,0 +1,36 @@
+// var: no reason to use var
+// let: allow it to reassign
+// const: do not allow it to reassign
+
+// let money= 25;
+// money= 60;
+// console.log(money);
+
+// let mango = 10;
+// mango= 20;
+
+// console.log(mango);
+// const mango = 30;
+// mango = 40;
+// console.log(mango);
+
+// let money = 50;
+// money = 40;
+// console.log(money);
+// const money = [20, 30, 50, 10];
+// money[2] = 100;
+// money.push(99)
+// console.log(money);
+// let persons = ['abdul', 'amir', 'tabla'];
+// persons.push('dobla')
+// console.log(persons);
+
+// object
+const student = {
+    name: 'Faisal',
+    class: 12,
+    roll: 1,
+}
+// console.log(student);
+student.name = ('Ismail')
+console.log(student);
