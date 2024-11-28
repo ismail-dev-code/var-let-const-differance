acb9013b7f03030f50b75ef0539b232194a13941
ismail-dev-code <ismail.online0385678@gmail.com>
2024-11-28 16:37:33 +0600

    destructuring

---
+++ destructuring.js
@@ -0,0 +1,12 @@
+const actor = {
+    name: 'Faisal',
+    age: 20,
+    phone: '018122000000',
+    money: 35689,
+}
+const phone = actor.phone;
+const name = actor.name;
+const age = actor.age;
+const money = actor.money;
+// console.log(actor.money);
+
