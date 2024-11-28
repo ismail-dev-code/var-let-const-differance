c7ded47829b008183fcacc3b13f1ff55ce772fb9
ismail-dev-code <ismail.online0385678@gmail.com>
2024-11-28 20:19:39 +0600

    keys.js

---
+++ keys.js
@@ -0,0 +1,17 @@
+const glass = {name: 'glass', 
+    color: 'golden',
+     price: 15,
+      isCleaned: true
+    };
+console.log(glass);
+const keys = Object.keys(glass);
+// console.log(keys);
+
+const values = Object.values(glass)
+// console.log(values);
+
+const entries = Object.entries(glass);
+// console.log(glass);
+
+// delete glass.isCleaned;
+console.log(glass);
