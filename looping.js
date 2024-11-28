3e97781742e9c018c2c4f118a7907d06700dc61f
ismail-dev-code <ismail.online0385678@gmail.com>
2024-11-28 21:00:39 +0600

    looping

---
+++ looping.js
@@ -0,0 +1,8 @@
+const numbers = [1, 6, 8, 9]
+    // for (let i = 0; i <numbers.length; i++){}
+    for(num of numbers){
+
+    }
+const nobab = 'siraj ud doula';
+for (const char of nobab);
+console.log(nobab);
