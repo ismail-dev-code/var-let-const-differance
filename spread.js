48386e91bb51c48b9378df90ff9171deb2ffd3d0
ismail-dev-code <ismail.online0385678@gmail.com>
2024-11-28 14:37:12 +0600

    spread.js

---
+++ spread.js
@@ -0,0 +1,15 @@
+const max = Math.max(2,34,41,4,6,7,)
+const numbers = [23,3,445,65,776,65];
+const arrayMax = Math.max(...numbers);
+
+// console.log(...numbers);
+console.log(arrayMax);
+
+// use spread operator to copy a object or array 
+const friends = [2, 4, 34, 3];
+const bondhu = friends;
+const dosto = [...friends]
+console.log(dosto);
+friends.push(100);
+console.log(friends);
+console.log(dosto);
