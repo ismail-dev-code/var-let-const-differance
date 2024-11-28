c43435d173c27cfd99358c719bb253a5bf842e3c
ismail-dev-code <ismail.online0385678@gmail.com>
2024-11-28 10:13:57 +0600

    arrow function

---
+++ arrow.js
@@ -0,0 +1,31 @@
+// function declaration *************
+
+// function add (a, b){
+//     const result = a + b;
+//     return result;
+// }
+// const sum = add(5, 60);
+// console.log(sum);
+
+// function add(a, b){
+//     return a+b;
+// }
+// const sum = add(5,100);
+// console.log(sum);
+
+// // function expression ***********
+// const add2 = function(a,b){
+//     return a+b;
+// }
+// const sum = add2(10,58);
+// console.log(sum);
+
+// arrow function ******** 
+// const add3 = (a, b) => a+b
+// const add4 = (num1, num2, num3, num4, num5) => num1+num2+num3+num4+num5
+// // const sum = add3(20,44);
+// const sum = add4(23,3,5,13,3)
+// console.log(sum);
+const multiply = (num1, num2) => num1*num2
+const multi = multiply(40, 2)
+console.log(multi);
